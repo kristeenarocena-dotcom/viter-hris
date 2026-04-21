ae16e22c9abaf314336fe917d45619295b1f2274
Arneldo Arocena <kristeenarocena@gmail.com>
2026-04-21 08:24:55 +0800

    updatesss

---
+++ rest/v1/controllers/developers/settings/users/create.php
@@ -0,0 +1,22 @@
+<?php
+// check database connection 
+$conn = null;
+$conn = checkDbConnection();
+// make use of classes for save database
+$val = new Users($conn);
+
+$val->users_is_active = 1;
+$val->users_first_name = trim($data['users_first_name']);
+$val->users_last_name = $data['users_last_name'];
+$val->users_email = $data['users_email'];
+$val->users_password = $data['users_password'];
+$val->users_role_id = $data['users_role_id'];
+$val->users_created = date("Y-m-d H:m:s");
+$val->users_updated = date("Y-m-d H:m:s");
+
+// validations
+isEmailExist($val, $val->users_email);
+
+$query = checkCreate($val);
+http_response_code(200);
+returnSuccess($val, "Users Create", $query);
